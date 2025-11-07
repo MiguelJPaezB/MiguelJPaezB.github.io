d25f34a3c68e8f2991dc7902a0e3261f06003c79
MiguelJPaezB <miguel.jpaezb07@gmail.com>
2025-11-06 23:21:52 -0500

    Adjustments to footer information, improvements and additions of resource styles for posts

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,21 +2,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     const container = document.getElementById('posts-container');
 
     try {
-        // Cargar los datos del archivo JSON
         const response = await fetch('posts/posts.json');
         const posts = await response.json();
 
-        // Limpiar el contenedor (por si hay algo previo)
         container.innerHTML = '';
 
-        // Generar una tarjeta para cada post
         posts.forEach(post => {
             const card = document.createElement('a');
             card.href = `posts/${post.file}`;
             card.classList.add('card-post');
 
             card.innerHTML = `
-                <img src="posts/banners/${post.banner}" alt="banner" class="banner">
+                <img src="posts/resources/${post.resources}/${post.banner}" alt="banner" class="banner">
                 <h4 class="title-post">${post.title}</h4>
                 <span class="short-description-post">${post.description}</span>
             `;
--- assets/js/recent-posts.js
+++ assets/js/recent-posts.js
@@ -2,23 +2,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     const container = document.getElementById('recent-posts');
 
     try {
-        // Cargar el JSON con todos los posts
         const response = await fetch('posts/posts.json');
         const posts = await response.json();
 
-        // Tomar los 3 últimos (asumiendo que el JSON está ordenado del más antiguo al más nuevo)
-        const recentPosts = posts.slice(-3).reverse(); // reverse() para que se muestren del más reciente al más antiguo
+        const recentPosts = posts.slice(-3).reverse();
 
         container.innerHTML = '';
 
-        // Crear una tarjeta por cada post
         recentPosts.forEach(post => {
             const card = document.createElement('a');
             card.href = `posts/${post.file}`;
             card.classList.add('card-post');
 
             card.innerHTML = `
-                <img src="posts/banners/${post.banner}" alt="banner" class="banner">
+                <img src="posts/resources/${post.resources}/${post.banner}" alt="banner" class="banner">
                 <h4 class="title-post">${post.title}</h4>
                 <span class="short-description-post">${post.description}</span>
             `;
